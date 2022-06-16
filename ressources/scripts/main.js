97d26e62a540f845f84be21c7fedaae9a91db1ab
baramex <contact@baramex.me>
2022-06-17 00:24:32 +0200

    remove autocomplete

--- ressources/scripts/main.js
+++ ressources/scripts/main.js
@@ -65,7 +65,6 @@ if (!sessionStorage.getItem("username") && getCookie("token")) {
 
 function update() {
     axios.get("/api/profiles/online").then(res => {
-        console.log(res);
         document.getElementById("online-count").innerText = res.data.length + " en ligne";
 
         var table = document.querySelector("#online-container > table");
